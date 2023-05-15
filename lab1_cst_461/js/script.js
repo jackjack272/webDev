516b051ae9fcb429fb66bb1db2c8af5ad0701319
jackjack272 <69359942+jackjack272@users.noreply.github.com>
2023-05-14 19:38:56 -0700

    lab 1 files

---
+++ lab1_cst_461/js/script.js
@@ -0,0 +1,10 @@
+
+// generate a page with a list of some random users. 
+
+//must construct the page and add pagination with each page showing 10 users. 
+// you DO NOT need to fetch data directly, but manually make a json file
+    // use 27 data objects.
+    
+//break the page into multiple profiles. 
+
+// 
