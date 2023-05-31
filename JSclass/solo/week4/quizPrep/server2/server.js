6d15281893dc0901314220ffe00e93659fd5d4c0
laptop <cristilive6@gmail.com>
2023-05-31 11:01:00 -0700

    quiz prep 2

--- JSclass/solo/week4/quizPrep/server2/server.js
+++ JSclass/solo/week4/quizPrep/server2/server.js
@@ -1,11 +1,16 @@
 let express=require("express")
 let body_parser= require("body-parser")
+
 let app= express();
+app.use(body_parser.urlencoded({extended:true}))
 
 
+let formRoute= require("./formRoutes.js")
+app.use("/form",formRoute)
 
 let route=require("./routes.js")
 app.use("/",route)
+
 
 app.listen(3000, ()=>{
     console.log("the application is running on port 3000")
