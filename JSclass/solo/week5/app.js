d9c7552a36563459aca0e087bcab9e28a7bb0ff8
cristi <cristilive6@gmail.com>
2023-06-03 11:02:56 -0700

    pug examples

--- JSclass/solo/week5/app.js
+++ JSclass/solo/week5/app.js
@@ -5,16 +5,40 @@ let port= 3000
 
 app.set('view engine',"pug")
 app.use(bodyParser.urlencoded({extended:true}))
+
 app.get("/cards",(req, res)=>{
-    res.render(__dirname+"/PUG/try.pug", {
+    res.render(__dirname+"/PUG/blanks.pug", {
         prompt:"worked",
         hint:"think about",
+    })
+})
+
+app.get("/loops", (req, res)=>{
+    let _users=["smith", "jhones", "bill" ,"tyler"]
+
+    res.render(__dirname+"/PUG/loops", {
+        getHeader: "This is a list of the users we're supposed to contact",
+        users:_users, 
+    })
+})
+
+app.get("/form",(req, res)=>{
+    res.render(__dirname+"/PUG/form",{
+        getTitle:"Try form page"
+    })
+})
+app.post("/pugForm", (req, res)=>{
+    res.render(__dirname+"/PUG/form2",{
+        greetMe:"hello "+req.body.username,
+        getPerson:req.body.username
     })
 })
 
 app.get("/",(req, res)=>{
     res.send("worked")
 })
+
+
 
 
 app.listen(port, ()=>{
