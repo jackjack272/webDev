343124f7e516377bb38a2754b3cba8ca6bcd6aad
laptop <cristilive6@gmail.com>
2023-06-14 15:18:11 -0700

    working on the planet app

--- lab3_solarSystem/Class Lab - Building Planet Card - start/app.js
+++ lab3_solarSystem/Class Lab - Building Planet Card - start/app.js
@@ -70,10 +70,57 @@ const planets = [
 // =============================================================
 
 // 1: Create a 'Planet' component that renders a planet card
-
+Planet =(props)=>{
+  return(
+      <div class="card">
+        <div>
+            <img src={props.url} alt={props.alt} ></img>
+        </div>
+        <h2>{props.name}</h2>
+        <p>{props.text}</p>
+        
+        <h3>Planet Profile</h3>
+        <ul>
+          <li><strong>Diameter: </strong> {props.diameter} </li>
+          <li><strong>Moons: </strong> {props.moons}  </li>
+        </ul>
+      </div>
+    );
+}
 
 // 2: Create a container component that iterates over the planets array 
 //    and renders a 'Planet' component for each object in the array 
+class App extends React.Component {
+
+  
+  
+  // render(){
+  //   return(
+  //     <div>
+  //       planets.map(singlePlanet =>{
+  //         <Planet 
+  //           id={singlePlanet.id}
+  //           name={singlePlanet.name}
+  //           diameter={singlePlanet.diameter}
+  //           moons={singlePlanet.moons}
+  //           text={singlePlanet.desc}          
+  //           url={singlePlanet.url}
+  //           alt={"image of "+singlePlanet.name}
+
+  //         />
+  //       })
+  //     </div>
+  //   );
+  // }
+
+}
+
+
+
 
 
 // 3: Render the container component to the DOM
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
