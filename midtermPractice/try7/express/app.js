68a871f4a2dd796e063e91aa73b89e5e0faa1c34
cristi <cristilive6@gmail.com>
2023-06-27 21:16:50 -0700

    7th attempt success express with pug templating

--- midtermPractice/try7/express/app.js
+++ midtermPractice/try7/express/app.js
@@ -1,17 +1,55 @@
-let express= require('express')
+let express= require("express")
+let bodyParser= require("body-parser")
 let app= express()
-let port=3000
+let port= 3000
 
-app.use(express.static("public"))
+//this will make the "name" values be able to be pulled form the form 
+app.use(bodyParser.urlencoded({extended:true}))
 
+// this will like the css
+app.use(express.static("public")) 
+
+app.set("view engine","PUG")
+
+let routes= require("./routes/routes.js")
+app.use("/abc",routes)
 
 app.get("/",(req,res)=>{
-    res.render(__dirname+"\\something.pug")
+    // console.log(__dirname+"\\views\\something.pug")
+    res.render(__dirname+"\\views\\something.pug")
+})
+
+app.get((req, res,next)=>{
+    console.log("inside of middleware")
+    
+    // throw "err"
+    next() // i will send to the next part of the execution 
+    
+})
+
+app.post("/",(req,res)=>{
+    console.log(req.body.age)
+    res.render(__dirname+"\\views\\something.pug",{
+        result:"i was passed"
+    })
+    
+})
+
+app.get((err, req, res, next)=>{
+    console.log("this is the catch middleware")
+    console.log("trigger "+err )
 })
 
 
+app.get("*",(req,res)=>{
+    res.send("404 page not found")
+})
 
 
 app.listen(port, ()=>{
-    console.log("server on port "+port)
+    console.log("app runnin on port "+port)
 })
+
+
+
+
