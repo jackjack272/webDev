3cdc6963fdd18c64c797239539525da257e02686
laptop <cristilive6@gmail.com>
2023-07-12 12:49:21 -0700

    week 9 just notes

---
+++ JSclass/class/week9/REST API Class Files/S15_planning_complete/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/quotes', (req, res)=>{
+  res.json({greeting: "Hello World!"});
+});
+
+// Send a GET request to /quotes to READ a list of quotes 
+// Send a GET request to quotes/:id to READ (view) a quote 
+// Send a POST request to /quotes to CREATE a new quote 
+// Send a PUT request to /quotes/:id to UPDATE (edit) a quote 
+// Send a DELETE request to /quotes/:id DELETE a quote Il Send a GET request to /quotes/quote/random to READ (view) a random quote
+
+
+app.listen(3000, () => console.log('Quote API listening on port 3000!'));
